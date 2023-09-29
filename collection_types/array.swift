b9e4803d9cb0bb7b5436353f5367dcd6e2fc116a
Fabio Cuomo <fabio.cuomo@gmail.com>
2023-09-29 10:23:43 +0200

    Add Arrays assignments

---
+++ collection_types/array.swift
@@ -0,0 +1,57 @@
+**Quiz 1: Array Basics**
+
+1. What is an array in programming?
+   a) A single value
+   b) A collection of values of the same type
+   c) A collection of values of different types
+
+2. In Swift, how do you declare an empty array of integers?
+   a) `var emptyArray = []`
+   b) `var emptyArray: [Int] = []`
+   c) `var emptyArray: Int[] = []`
+
+3. Can you have duplicate values in an array?
+   a) Yes
+   b) No
+
+**Exercise 1: Array Initialization**
+
+Declare an array called `fruits` and initialize it with the following fruits: "Apple," "Banana," "Orange," and "Grapes."
+
+**Quiz 2: Array Operations**
+
+4. How do you add a new element to the end of an array in Swift?
+   a) Using `append(_:)`
+   b) Using `insert(_:at:)`
+   c) Using `add(_:)`
+
+5. What is the result of `fruits.count` after adding one more fruit to the `fruits` array from Exercise 1?
+   a) 3
+   b) 4
+   c) 5
+
+6. How do you access the second element in an array called `myArray` in Swift?
+   a) `myArray[1]`
+   b) `myArray[0]`
+   c) `myArray[2]`
+
+**Exercise 2: Array Manipulation**
+
+Add "Cherry" to the beginning of the `fruits` array and remove "Banana" from it.
+
+**Quiz 3: Searching in Arrays**
+
+7. Which method can you use to check if an array contains a specific element?
+   a) `findElement(_:)`
+   b) `contains(_:)`
+   c) `search(_:)`
+
+8. What does the `index(of:)` method return if the element is not found in the array?
+   a) nil
+   b) -1
+   c) 0
+
+**Exercise 3: Array Searching**
+
+Check if the `fruits` array contains "Grapes." If it does, print "Grapes found," otherwise print "Grapes not found."
+
